3723676b1351660289e9b5fcd8a33b6c2aefd019
Lucas Manuel Alface <112022654+Desenvolvedormoz@users.noreply.github.com>
2022-09-11 11:21:25 +0200

    Novo codiigo inserido e aplicativo cocluido

--- novo_aplicativo/App.js
+++ novo_aplicativo/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ estilos.container }>
+      <Image
+        source={ { uri: 'https://www.devmedia.com.br/arquivos/cursos/rn_exibindo_imagem/aula_3.png'} }
+        style={ estilos.img }
+      />
+
+      <Text style={ estilos.paragrafo}>
+        Queen é o nome da lendária banda britânica, criada em 1970 por Freddie Mercury e dois ex-músicos do Smile, Brian May e Roger Taylor. A banda começou com o hard rock, mas depois mudou seu estilo para pop-rock, o que tornou seu nome icônico.
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  img: {
+    width: 400,
+    height: 200
+  },
+
+  paragrafo: {
+    marginTop: 20,
+    textAlign: 'center',
+  }
 });
