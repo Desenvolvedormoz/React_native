555fb099d7f7c9bd7189cf25f1beb28a1d6046a2
Lucas Manuel Alface <112022654+Desenvolvedormoz@users.noreply.github.com>
2022-09-11 12:32:59 +0200

    App concluido

--- app_novo/App.js
+++ app_novo/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import Capa from './capa.png'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <Text style={estilos.titulo}>Guerra do amanhã</Text>
+      <Text style={estilos.descricao}>Gêneros: Ação e fixão</Text>
+      <Image source={Capa} style={estilos.imagem}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titulo: {
+    fontSize: 22,
+  },
+
+  descricao: {
+    marginBottom: 20,
+  },
+
+  imagem: {
+      width: 300,
+      height: 300,
+  }
 });
